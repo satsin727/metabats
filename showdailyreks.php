fc3fae80e4591b0267ea4539f49ac365fc87ddc3
unknown <satsin727@gmail.com>
2022-10-06 04:54:12 -0500

    fixed eci trackcm issue

--- showdailyreks.php
+++ showdailyreks.php
@@ -41,7 +41,7 @@ $uid = $dta['uid'];
 				<div class="panel panel-default">
           <div class="panel-heading"> <a href="admin.php?action=showreports"><button name="back" class="btn btn-primary">Back</button></a></div>
           <div class="panel-body">
-						<table data-toggle="table"  data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="false" data-sort-name="name" data-sort-order="asc">
+						<table data-toggle="table"  data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="false" data-sort-name="ID" data-sort-order="asc">
 						    <thead>
 						    <tr>
                                         <th data-field="ID">ID</th>
