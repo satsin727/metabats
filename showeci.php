d1d01d24ecb80daee2254aac57d51677d1c5c51e
unknown <satsin727@gmail.com>
2022-10-09 18:55:05 -0500

    fixed eci display

--- showeci.php
+++ showeci.php
@@ -64,8 +64,8 @@ $data = $ins->fetchAll();
 						        <th data-field="client"  data-sortable="true">Client</th>
 						        <th data-field="ECI"  data-sortable="true">ECI's Numbers</th> 
 						        <th data-field="comment"  data-sortable="true">Comment</th>
-								<?php   if($dta['level'] == 1 || $dta['level'] == 2) {	?>   <th data-field="name" data-sortable="true">Add Client</th> <?php }	?> 
-						        
+								<!-- <?php   if($dta['level'] == 1 || $dta['level'] == 2) {	?>   <th data-field="name" data-sortable="true">Add Client</th> <?php }	?> 
+-->
 						    </tr>
 						    </thead>
 						   <tbody>
@@ -132,11 +132,11 @@ $eci_num = $conn->query("SELECT COUNT(*) FROM `app_data` WHERE `reqid`= $reqid a
 		<td> <?php echo $eci_num; ?>&nbsp;&nbsp;&nbsp;<a href="ecidetails.php?eciid=<?php echo $app_id; ?>"><button name="ecidetails" class="btn btn-primary">Show ECI Details</button></a></td>
 		<td> <a href="comments.php?ecicom_id=<?php echo $app_id; ?>" onclick="window.open(this.href,'popupwindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,height=400,width=400,'); return false;"><button name="viewcomment" class="btn btn-primary">View Comment</button></a> 
 		<a href="addcomment.php?ecicom_id=<?php echo $app_id; ?>" onclick="window.open(this.href,'popupwindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,height=400,width=400,addressbar=no'); return false;"><button name="addcomment" class="btn btn-primary">Add Comment</button></a></td> 
-	
+	<!--  
 		<?php   if($dta['level'] == 1 || $dta['level'] == 2) {	?> <td> 	
-  <!--  		<a href="appdata_cmd.php?do=edit&id=<?php echo $row['app_id']; ?>"><img src="images/b_edit.png" alt="Edit" width="16" height="16" border="0" title="Edit" /></a>
-    				<a href ="appdata_cmd.php?do=delete&subid=<?php echo $row['app_id']; ?>" onClick="return confirm('Are you sure you want to remove this submission ?')"><img src="images/b_drop.png" alt="Delete" width="16" height="16" border="0" title="Delete"/></a> -->
-    	</td><?php } ?>
+  		<a href="appdata_cmd.php?do=edit&id=<?php echo $row['app_id']; ?>"><img src="images/b_edit.png" alt="Edit" width="16" height="16" border="0" title="Edit" /></a>
+    				<a href ="appdata_cmd.php?do=delete&subid=<?php echo $row['app_id']; ?>" onClick="return confirm('Are you sure you want to remove this submission ?')"><img src="images/b_drop.png" alt="Delete" width="16" height="16" border="0" title="Delete"/></a> 
+    	</td><?php } ?> -->
     </tr>			
     <?php
 }
