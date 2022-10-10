4866029a0f2f56bf89200a76977f828552438c2d
unknown <satsin727@gmail.com>
2022-10-09 21:26:25 -0500

    added client name and company domain in all reqs page

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 ob_start();
 session_start();
-ini_set( "display_errors", true );
+ini_set( "display_errors", false );
 date_default_timezone_set("America/Chicago");
 
-define( "DB_DSN", "mysql:host=162.241.80.6;dbname=oejwxwmy_bats" );
+define( "DB_DSN", "mysql:host=localhost;dbname=oejwxwmy_bats" );
 define( "DB_USERNAME", "oejwxwmy_bats" );
 define( "DB_PASSWORD", "m3+@h0riz0n!" );
 
