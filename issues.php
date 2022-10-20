ac46df0006884dbcc3102e5009e7dbf4288dc9c9
unknown <satsin727@gmail.com>
2022-10-20 18:27:19 -0500

    download monthly app/sub/rc button active

--- issues.php
+++ issues.php
@@ -37,7 +37,7 @@ $data = $ins->fetchAll();
 <div class="row">
 			<ol class="breadcrumb">
 				<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
-				<li class="active">Escalations</li>
+				<li class="active">Notices - Issues/Escalations/Process Violation</li>
 			</ol>
 </div>
 <div class="row">
