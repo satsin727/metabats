aa8fd6ee9751cb1c6690718c2f291131240d1baf
unknown <satsin727@gmail.com>
2023-06-19 16:02:03 -0500

    added req id in all reqs

--- reqnoedit.php
+++ reqnoedit.php
@@ -54,7 +54,7 @@ if(isset($_SESSION['username']) && $dta['sess']==$_SESSION['username'])
 			<tr>
 			<br><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
 			<br><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td class="button">
-		<button type="buuton" name="update">Update</button> </td>
+		<button type="button" name="update">Update</button> </td>
 			</tr>
 		</form>
 
