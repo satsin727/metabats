33778e6402d7ff4233bee538960d9de606e1f7ac
unknown <satsin727@gmail.com>
2023-06-28 17:37:25 -0500

    added reqs download page

--- reqsdownload.php
+++ reqsdownload.php
@@ -58,7 +58,7 @@ $data = $ins->fetchAll();
 
                             $jd = $conn->query("select rdesc from jd where reqid = $reqid")->fetchColumn();
                         $jdtext = strip_tags($jd);
-                        $jdtext = html_entity_decode($jd);
+                        $jdtext = htmlspecialchars_decode($jdtext);
 
                         $clientname = $conn->query("select rend_client from req where reqid = $reqid")->fetchColumn();
 
