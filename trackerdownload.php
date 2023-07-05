a7cd4f3eee72d055c358d02b86ddd3d82427f612
unknown <satsin727@gmail.com>
2023-07-05 16:08:09 -0500

    Yearly app download page

--- trackerdownload.php
+++ trackerdownload.php
@@ -296,16 +296,31 @@ $data = $ins->fetchAll();
                                 $ureq_id = "W".$curweek.$cur_date."-".$ureq_id;
                         */
                                 $cid = $row['client_id'];
-                        
-                        $bpcompany = $conn->query("SELECT companyname from clients where `cid` = $cid")->fetchColumn();
-                        $bpname = $conn->query("SELECT rname from clients where `cid` = $cid")->fetchColumn();
+
+                                $cq = "SELECT * from clients where `cid` = $cid";
+                                $pq1= $conn->prepare($cq);
+                                $pq1->execute(); 
+                                $cdata = $pq1->fetch();
+                        $bpcompany = $cdata['companyname'];
+                        $bpname = $cdata['rname'];
+                        $bpemail = $cdata['remail'];
+                        $bpphone = $cdata['rphone'];
+                        $bplocation = $cdata['rlocation'];
+                        $bptier = $cdata['tier'];
+                        $bptimezone = $cdata['rtimezon'];
+                     /*   $bpname = $conn->query("SELECT rname from clients where `cid` = $cid")->fetchColumn();
                         $bpemail = $conn->query("SELECT remail from clients where `cid` = $cid")->fetchColumn();
                         $bpphone = $conn->query("SELECT rphone from clients where `cid` = $cid")->fetchColumn();                        
                         $bplocation = $conn->query("SELECT rlocation from clients where `cid` = $cid")->fetchColumn();
                         $bptier = $conn->query("SELECT tier from clients where `cid` = $cid")->fetchColumn();      
-                        $bptimezone = $conn->query("SELECT rtimezon from clients where `cid` = $cid")->fetchColumn();    
-                        $bpuid = $conn->query("SELECT `uid` from clients where `cid` = $cid")->fetchColumn();
-                        $bpsm = $conn->query("SELECT `name` from users where `uid` = $bpuid")->fetchColumn();                       
+                        $bptimezone = $conn->query("SELECT rtimezon from clients where `cid` = $cid")->fetchColumn();    */
+                        $bpuid = $cdata['uid'];
+
+                            $smq = "SELECT * from users where `uid` = $bpuid";
+                            $pq= $conn->prepare($smq);
+                            $pq->execute(); 
+                            $smdata = $pq->fetch();
+                        $bpsm = $smdata['name'];                       
                                 $app_id = $row['app_id'];
                         $ipname = $conn->query("select t1ip_name from app_data where app_id = $app_id")->fetchColumn();
                         $clientname = $conn->query("select rend_client from req where reqid = $reqid")->fetchColumn();
