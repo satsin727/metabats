4550669c0f629835df6aa072930d04d8823d777d
Satish <satsin727@gmail.com>
2024-03-04 09:56:21 -0600

    created consultant data page

--- updatedhotlist.php
+++ updatedhotlist.php
@@ -78,7 +78,7 @@ $sid = $row['skill'];
 	?>
     <tr>
   		<td data-order="<?php echo $i; ?>"> <?php echo $i; $i=$i+1;  ?></td>
-    	<td data-search="<?php echo $row['cfname']." ".$row['clname']; ?>"> <a href="assigneddetails.php?id=<?php echo $row['cid']; ?>"><?php echo $row['cfname']." ".$row['clname']; ?></a></td>
+    	<td data-search="<?php echo $row['cfname']." ".$row['clname']; ?>"> <?php echo $row['cfname']." ".$row['clname']; ?></td>
     	<td data-search="<?php echo $dta2['skillname']; ?>"> <?php echo $dta2['skillname']; ?></td>    	
     	<td data-search="<?php echo $row['cmlocation']; ?>"> <?php echo $row['cmlocation']; ?></td> 	
     	<td data-search="<?php echo $row['relocation']; ?>"> <?php echo $row['relocation']; ?></td>     
