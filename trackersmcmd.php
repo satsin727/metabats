5a87ba2d479e50ae56834a2cbb843db3715eb314
Satish <satsin727@gmail.com>
2024-07-04 12:08:35 -0500

    chnaged made by satish

--- trackersmcmd.php
+++ trackersmcmd.php
@@ -20,12 +20,17 @@ $dta = $ins->fetch();
 $curdate =date('Y-m-d');
 
 $olddate=0;
-if(isset($_POST['date']) || isset($_GET['date']))
+if(isset($_POST['date']))
 {
     $cdate = $_POST['date'];
     $olddate = 1;
     $cdate = strtotime($cdate);
     $curdate =date('Y-m-d',$cdate);
+}
+else if(isset($_GET['date']))
+{
+	$olddate = 1;
+    $curdate =date('Y-m-d');
 }
 else {
     $curdate =date('Y-m-d');
