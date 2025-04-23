842ddaa8fa4ae79b044f12697cc09d76d00a9988
satsin727 <satsin727@gmail.com>
2025-04-23 17:52:58 -0500

    fixing comments in downloaded reqs

--- reqsdownload.php
+++ reqsdownload.php
@@ -186,7 +186,7 @@ $data = $ins->fetchAll();
 							else {
 									$sreqid = $reqid;
 									$ssmid = $uid ;
-                                    $com_query = "SELECT * FROM comments WHERE (com_postid = $reqid AND reqcom_id = 1) OR (com_postid = $appid AND appcom_id = 1) OR (com_postid = $appid AND rccom_id = 1) OR (com_postid = $appid AND subcom_id = 1) OR (com_postid = $appid AND ecicom_id = 1) order by uid asc";
+                                    $com_query = "SELECT * FROM comments WHERE uid = $uid and ((com_postid = $reqid AND reqcom_id = 1) OR (com_postid = $appid AND appcom_id = 1) OR (com_postid = $appid AND rccom_id = 1) OR (com_postid = $appid AND subcom_id = 1) OR (com_postid = $appid AND ecicom_id = 1))";
                                     $cins= $conn->prepare($com_query);
                                     $cins->execute();
                                     $commentdata = $cins->fetchAll();
