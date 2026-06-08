f8c8d5c162457e4afb4a10c8c4aea23772689a0b
Satish <satsin727@gmail.com>
2026-06-07 19:45:26 -0500

    Updated production changes

--- showapplication.php
+++ showapplication.php
@@ -44,7 +44,7 @@ if($dta['level'] == 1 || $dta['level'] == 2 || $dta['level'] == 3)
 		}
 		if($dta['level'] == 2)
 		{
-			$query = "select * from app_data AS A Inner JOIN consultants AS B ON A.consultant_id = B.cid Inner JOIN users AS C ON A.uid = C.uid where A.status =1 and C.rmid = $smid and rcdone= 0  and subdone= 0 and TO_DAYS('$curdate') - TO_DAYS(appdate) <= 3 order by appdate desc";
+			$query = "select * from app_data AS A Inner JOIN consultants AS B ON A.consultant_id = B.cid Inner JOIN users AS C ON A.uid = C.uid where A.status =1 and rcdone= 0  and subdone= 0 and TO_DAYS('$curdate') - TO_DAYS(appdate) <= 3 order by appdate desc";
 		}
 		if($dta['level'] == 3)
 		{
